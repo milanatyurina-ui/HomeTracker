a5136ec8dce691a73b40671674185e4e7f284426
Milana <melany.klav@mail.ru>
2026-03-19 14:09:08 +0300

    добавлена возможность удаления записей и поиска записей по id

--- backend/route.py
+++ backend/route.py
@@ -19,3 +19,16 @@ async def add_task(
 async def get_tasks() -> list[STask]:
     tasks = await TaskRepository.find_all()
     return tasks
+
+@router.get("/{task_id}")
+async def get_task(task_id: int) -> STask:
+    task = await TaskRepository.find_by_id(task_id)
+    return task
+
+@router.delete("/{task_id}")
+async def delete_task(task_id: int) -> None:
+    flag = await TaskRepository.delete_one(task_id)
+    if(flag == True):
+        return {"ok": True}
+    else:
+        return None
